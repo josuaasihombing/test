fb6b147d7a56dd21020ff103332889e8893e4495
Josua A Sihombing <i_yosch@yahoo.com>
2016-06-13 13:06:23 +0700

    test

---
+++ app/src/main/java/com/example/rnd_03/test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rnd_03.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/* this is the test file */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
